61a2f2b03d59be323f851acd50ca1ae3bf28766d
Tyler Neil <17966029+nicho166@users.noreply.github.com>
2022-11-17 16:26:28 -0500

    New functions and fun things added to the HTML doc

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function changeColor(color){
     console.log(heading);
     heading.textContent = "Haha, this works!"
 }
+
+const name = document.querySelector("#first_name")
+const greetMeButton = document.querySelector(".greet-btn");
+const greetingOutput = document.querySelector(".greeting")
+
+greetMeButton.addEventListener("click", (event) => {
+    greetingOutput.innerHTML = `Hello ${name.value}`;
+})
