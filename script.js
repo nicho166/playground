f42dd711358eccd1b25d7e9e35b51f8f4c44c13a
Tyler Neil <17966029+nicho166@users.noreply.github.com>
2025-04-11 09:33:53 -0400

    Just making a commit, nothing special

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-function changeColor(color){
-    const heading = document.getElementById("header");
-    console.log(heading);
-    heading.textContent = "Haha, this works!"
-}
-
-const name = document.querySelector("#first_name")
-const greetMeButton = document.querySelector(".greet-btn");
-const greetingOutput = document.querySelector(".greeting")
-
-greetMeButton.addEventListener("click", (event) => {
-    greetingOutput.innerHTML = `Hello ${name.value}`;
-})
+document.addEventListener('DOMContentLoaded', function() {
+    const headerLogo = document.querySelector('.header-logo a');
+    headerLogo.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default anchor behavior
+        headerLogo.style.color = 'green';
+    });
+});
