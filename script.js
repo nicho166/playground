9856118139b892c6a336cb26c3fd368811a3bf63
Tyler Neil <17966029+nicho166@users.noreply.github.com>
2025-04-24 12:56:58 -0400

    some changes to script.js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
         headerLogo.style.color = 'green';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const visualContainer = document.querySelector('.visual-container');
+    const h1 = visualContainer.querySelector('h1');
+    visualContainer.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default anchor behavior
+        h1.style.color = 'green';
+    });
+});
