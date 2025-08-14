bdc5755bf1499110625ded5fa1d74a372e8d56cd
Tyler Neil <17966029+nicho166@users.noreply.github.com>
2025-08-14 16:14:11 -0400

    added some new features

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+/* JS for my to-do list */
+const input = document.querySelector("#list-input");
+const button = document.querySelector("#submit");
+const toDoList = document.querySelector("#to-do-list");
+
+
+button.addEventListener("click", () => {
+  const text = input.value.trim();
+
+  if (text){
+    const item = document.createElement("li");
+    item.textContent = text;
+    item.addEventListener('click', addLineThrough(item));
+    toDoList.appendChild(item);
+    input.value = '';
+  }
+})
+
+const listItem = document.querySelector("li");
+
+function addLineThrough(el){
+  el.style.textDecoration = 'line-through';
+}
+
+/* JS for my moving red dot */
 const container = document.querySelector('.visual-container');
 const dot = document.querySelector('#dot');
 const step = '5px';
@@ -29,3 +54,4 @@ function moveDot(event){
     dot.style.top = `100px`;
   }
 }
+
